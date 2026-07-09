a67f3cf9f24417c23417ce9dad35315a94444113
Akasha0729 <IT26102131@my.sliit.lk>
2026-07-09 07:45:17 +0530

    Add IT26102131Lab2Q2.java to Q2

---
+++ Q2/IT26102131Lab2Q2.java
@@ -0,0 +1,12 @@
+public class IT26102131Lab2Q2{  // STARTING OF THE CLASS
+	
+	public static void main (String []args){
+		
+		double length = 10;
+		double perimeter = 4*length;
+		double radius = perimeter /(2.0*3.14);
+		
+		System.out.println("Radius of the circular fence: " +radius);
+		
+	} // END OF MAIN METHOD
+} // ENDING THE CLASS 
