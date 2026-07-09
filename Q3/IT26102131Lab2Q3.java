91a6067d22dbf7625447a64fef46129bd2873767
Akasha0729 <IT26102131@my.sliit.lk>
2026-07-09 07:50:42 +0530

    Add IT26102131Lab2Q3.java to Q3

---
+++ Q3/IT26102131Lab2Q3.java
@@ -0,0 +1,13 @@
+public class IT26102131Lab2Q3{  // STARTING OF THE CLASS
+
+    public static void main(String[] args) {
+		
+	   double sideA= 3.0;
+	   double sideB= 4.0;
+	   
+	   double hypotenuse= Math.sqrt(sideA*sideA+sideB*sideB);
+	   
+	   System.out.println("Length of the hypotenuse:" +hypotenuse);
+	   
+    }  // END OF MAIN METHOD
+}  // ENDING THE CLASS 	   
