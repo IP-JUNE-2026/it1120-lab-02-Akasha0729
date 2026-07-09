2e2bc99b39c88503a07aef6131cc83575b61d413
Akasha0729 <IT26102131@my.sliit.lk>
2026-07-09 07:43:06 +0530

    Add Lab2Q1.java to Q1

---
+++ Q1/IT26102131Lab2Q1.java
@@ -0,0 +1,14 @@
+public class IT26102131Lab2Q1 {  // STRTING THE CLASS
+
+    public static void main(String[] args){
+
+        double perimeter= 100;
+       	double length= (perimeter*2)/7;
+		double width= (3.0/4)*length;
+		
+		System.out.println("Length of the fence:"+length);
+		System.out.println("Width of the fence:"+width);
+		
+		}  // END OF MAIN METHOD
+}  // ENDING THE CLASS		
+	
